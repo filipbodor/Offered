77d35f4711f7c6bfcd4ac60576f82e973ddf13e4
Fylybg0 <bodorfilip8@gmail.com>
2024-04-27 15:19:54 +0200

    navigation menu translated into slovak

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,5 +1,5 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
-    <!-- Primary Navigation Menu -->
+    <!-- Hlavné navigačné menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-20">
             <div class="flex">
@@ -10,22 +10,22 @@
                     </a>
                 </div>
 
-                <!-- Navigation Links -->
+                <!-- Navigačné odkazy -->
 
             </div>
 
             @auth
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <!-- Settings Dropdown -->
-                <div class="ms-3 relative">
-                    <x-dropdown align="right" width="48">
-                        <x-slot name="trigger">
-                            @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                                <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                    <img class="h-10 w-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
-                                </button>
-                            @else
-                                <span class="inline-flex rounded-md">
+                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                    <!-- Nastavenia Dropdown -->
+                    <div class="ms-3 relative">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                                    <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                        <img class="h-10 w-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                                    </button>
+                                @else
+                                    <span class="inline-flex rounded-md">
                                     <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
                                         {{ Auth::user()->name }}
 
@@ -34,50 +34,50 @@
                                         </svg>
                                     </button>
                                 </span>
-                            @endif
-                        </x-slot>
+                                @endif
+                            </x-slot>
 
-                        <x-slot name="content">
-                            <!-- Account Management -->
-                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Account') }}
-                            </div>
+                            <x-slot name="content">
+                                <!-- Správa účtu -->
+                                <div class="block px-4 py-2 text-xs text-gray-400">
+                                    {{ __('Správa účtu') }}
+                                </div>
 
-                            <x-dropdown-link href="{{ route('profile.show') }}">
-                                {{ __('Profile') }}
-                            </x-dropdown-link>
-
-                            <x-dropdown-link href="{{ route('advertisements.create') }}">
-                                {{ __('Ponúknuť') }}
-                            </x-dropdown-link>
-
-                            @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                                <x-dropdown-link href="{{ route('api-tokens.index') }}">
-                                    {{ __('API Tokens') }}
+                                <x-dropdown-link href="{{ route('profile.show') }}">
+                                    {{ __('Profil') }}
                                 </x-dropdown-link>
-                            @endif
 
-                            <div class="border-t border-gray-200"></div>
-
-                            <!-- Authentication -->
-                            <form method="POST" action="{{ route('logout') }}" x-data>
-                                @csrf
-
-                                <x-dropdown-link href="{{ route('logout') }}"
-                                         @click.prevent="$root.submit();">
-                                    {{ __('Log Out') }}
+                                <x-dropdown-link href="{{ route('advertisements.create') }}">
+                                    {{ __('Ponúknuť') }}
                                 </x-dropdown-link>
-                            </form>
-                        </x-slot>
-                    </x-dropdown>
+
+                                @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                                    <x-dropdown-link href="{{ route('api-tokens.index') }}">
+                                        {{ __('API tokeny') }}
+                                    </x-dropdown-link>
+                                @endif
+
+                                <div class="border-t border-gray-200"></div>
+
+                                <!-- Odhlásenie -->
+                                <form method="POST" action="{{ route('logout') }}" x-data>
+                                    @csrf
+
+                                    <x-dropdown-link href="{{ route('logout') }}"
+                                                     @click.prevent="$root.submit();">
+                                        {{ __('Odhlásiť sa') }}
+                                    </x-dropdown-link>
+                                </form>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
                 </div>
-            </div>
             @else
                 <div class="flex">
                     @if (Route::has('login'))
                         <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex pe-4">
                             <x-nav-link href="{{ route('login') }}" :active="request()->routeIs('login')">
-                                {{ __('Login') }}
+                                {{ __('Prihlásiť sa') }}
                             </x-nav-link>
                         </div>
                     @endif
@@ -85,7 +85,7 @@
                     @if (Route::has('register'))
                         <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex pe-4">
                             <x-nav-link href="{{ route('register') }}" :active="request()->routeIs('register')">
-                                {{ __('Register') }}
+                                {{ __('Registrovať sa') }}
                             </x-nav-link>
                         </div>
                     @endif
@@ -104,9 +104,9 @@
         </div>
     </div>
 
-    <!-- Responsive Navigation Menu -->
+    <!-- Responzívne navigačné menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <!-- Responsive Settings Options -->
+        <!-- Responzívne možnosti nastavení -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             @auth
                 <div class="flex items-center px-4">
@@ -123,29 +123,28 @@
                 </div>
 
                 <div class="mt-3 space-y-1">
-                    <!-- Account Management -->
+                    <!-- Správa účtu -->
                     <x-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
-                        {{ __('Profile') }}
+                        {{ __('Profil') }}
                     </x-responsive-nav-link>
 
                     <x-responsive-nav-link href="{{ route('advertisements.create') }}" :active="request()->routeIs('advertisements.create')">
                         {{ __('Ponúknuť') }}
                     </x-responsive-nav-link>
 
-
                     @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
                         <x-responsive-nav-link href="{{ route('api-tokens.index') }}" :active="request()->routeIs('api-tokens.index')">
-                            {{ __('API Tokens') }}
+                            {{ __('API tokeny') }}
                         </x-responsive-nav-link>
                     @endif
 
-                    <!-- Authentication -->
+                    <!-- Odhlásenie -->
                     <form method="POST" action="{{ route('logout') }}" x-data>
                         @csrf
 
                         <x-responsive-nav-link href="{{ route('logout') }}"
-                                       @click.prevent="$root.submit();">
-                            {{ __('Log Out') }}
+                                               @click.prevent="$root.submit();">
+                            {{ __('Odhlásiť sa') }}
                         </x-responsive-nav-link>
                     </form>
 
@@ -153,13 +152,13 @@
             @else
                 @if (Route::has('login'))
                     <x-responsive-nav-link href="{{ route('login') }}"  :active="request()->routeIs('login')">
-                        {{ __('Login') }}
+                        {{ __('Prihlásiť sa') }}
                     </x-responsive-nav-link>
                 @endif
 
                 @if (Route::has('register'))
                     <x-responsive-nav-link href="{{ route('register') }}"  :active="request()->routeIs('register')">
-                        {{ __('Register') }}
+                        {{ __('Registrovať sa') }}
                     </x-responsive-nav-link>
                 @endif
             @endauth
